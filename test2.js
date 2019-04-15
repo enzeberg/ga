cb578def25972d9c0d005ffbcf8a0162e3031c52
Li Enze <enzeberg@gmail.com>
2019-04-15 21:34:43 +0800

    Add files via upload

--- test2.js
+++ test2.js
@@ -1,15 +1,15 @@
 const ga = require('./ga.js');
 const printElites = require('./print_elites');
 
-// Problem 3: 求 f(x) = x + sin(x) + cos(x), (0 <= x <= 10) 的最大值
+// Problem 3: 求 f(x) = x^2 + sin(x) + cos(x), (-5 <= x <= 5) 的最大值
 const bounds = [
   {
-    lower: 0,
-    upper: 10,
+    lower: -5,
+    upper: 5,
   },
 ];
 function fitnessFun(arr) {
-  return arr[0] + Math.sin(arr[0]) + Math.cos(arr[0]);
+  return Math.pow(arr[0], 2) + Math.sin(arr[0]) + Math.cos(arr[0]);
 }
 const elites = ga(30, 100, 0.5, 0.05, bounds, fitnessFun, 50);
 
